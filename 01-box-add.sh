da3a8bf736f071e399b8ac9e3ba6ca830e35c476
Eric Ahn <byungwook.ahn@gmail.com>
2014-01-15 16:05:50 +0900

    Update 01-box-add.sh

--- 01-box-add.sh
+++ 01-box-add.sh
@@ -2,4 +2,4 @@
 #vagrant box add openplatform-base http://files.vagrantup.com/precise64.box
 echo "######################################################################"
 echo "vagrant box add openplatform-base ./templates/precise64.box"
-vagrant box add openplatform-base ../templates/precise64.box 
+vagrant box add openplatform-base ./templates/precise64.box 
